2c5c5adc3184388f79f0034bcc00adc3d31d6098
Noah Anderson <noahanderson0414@gmail.com>
2026-02-23 09:54:31 -0500

    Add position_history to Snake and draw the body of the Snake.

--- modules/snake.py
+++ modules/snake.py
@@ -3,13 +3,22 @@ import pygame
 class Snake:
     def __init__(self):
         self.position = pygame.Vector2()
+        self.position_history = []
         self.direction = pygame.Vector2(1.0, 0.0)
         self.speed = 200.0
+        self.length = 100
 
     def update(self, delta_time: float) -> None:
+        # Add the old position to position_history.
+        self.position_history.append(pygame.Vector2(self.position.x, self.position.y))
+        if len(self.position_history) >= self.length:
+            self.position_history.pop(0)
+
         # Integrate our velocity (direction * magnitude) to position.
         self.position += self.direction * self.speed * delta_time
 
     def draw(self, surface: pygame.Surface) -> None:
-        # Draw a circle at the position of the Snake.
-        pygame.draw.circle(surface, pygame.Color(255, 255, 255, 255), self.position, 50.0)
+        # Draw a circle at the current position of the Snake and all positions in the history.
+        pygame.draw.circle(surface, pygame.Color(255, 255, 255, 255), self.position, 25.0)
+        for position in self.position_history:
+            pygame.draw.circle(surface, pygame.Color(255, 255, 255, 255), position, 25.0)
